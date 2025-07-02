1cb0aacd07ace43925388c378adb3a7e183c6dff
sebhdz <olguinsebas05@gmail.com>
2025-07-01 23:39:26 -0600

    creacion basica de la api

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
+type album struct {
+	Nombre  string `json:"nombre"`
+	Artista string `json:"artist"`
+}
+
+var albumes = []album{
+	{Nombre: "Tu hogar", Artista: "Tino el pinguino"},
+	{Nombre: "Lo legal", Artista: "El bebeto"},
+}
+
+func Pong(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albumes)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/ping", Pong)
+	router.Run(":8080")
 }
